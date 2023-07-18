b8cea9f79ad0b8fba49aa24af115bb230f3e1f1b
Osigelialex <osigelialex@gmail.com>
2023-07-18 20:38:43 +0100

    update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,6 +6,7 @@ functions:
   validate(line): checks if line meets requirement
 """
 import re
+import sys
 
 
 def validate(line):
@@ -18,42 +19,40 @@ def validate(line):
         r"\/projects\/260 HTTP\/1\.1\" (\d{3}) (\d+)$"
     return re.match(pattern, line)
 
-if __name__ == "__main__":
-    import sys
 
-    total_file_size = 0
-    line_count = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+total_file_size = 0
+line_count = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
-    try:
-        for line in sys.stdin:
-            if line_count == 10:
-                print(f"File size: {total_file_size}")
-                for code, count in status_codes.items():
-                    if count > 0:
-                        print(f"{code}: {count}")
-                line_count = 0
+try:
+    for line in sys.stdin:
+        if line_count == 10:
+            print(f"File size: {total_file_size}")
+            for code, count in status_codes.items():
+                if count > 0:
+                    print(f"{code}: {count}")
+            line_count = 0
 
-            if validate(line):
-                line_count += 1
-                total_file_size += int(line.split()[-1])
-                code = line.split()[-2]
-                status_codes[code] = status_codes[code] + 1
-            else:
-                line_count += 1
-                pass
+        if validate(line):
+            line_count += 1
+            total_file_size += int(line.split()[-1])
+            code = line.split()[-2]
+            status_codes[code] = status_codes[code] + 1
+        else:
+            line_count += 1
+            pass
 
-    except KeyboardInterrupt:
-        print(f"File size: {total_file_size}")
-        for code, count in status_codes.items():
-            if count > 0:
-                print(f"{code}: {count}")
+except KeyboardInterrupt:
+    print(f"File size: {total_file_size}")
+    for code, count in status_codes.items():
+        if count > 0:
+            print(f"{code}: {count}")
