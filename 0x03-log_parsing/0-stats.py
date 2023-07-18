61ca6311e92ea3ea39ff99d9d3a8264fdd26a7b6
Osigelialex <osigelialex@gmail.com>
2023-07-18 20:48:14 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
 """
 A module for solving the log parsing problem
-
-functions:
-  validate(line): checks if line meets requirement
 """
-import re
-import sys
-
-
-def validate(line):
-    """checks if line matches required pattern
-
-    Args:
-        line (string): line read from stdin
-    """
-    pattern = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.+)\] \"GET " \
-        r"\/projects\/260 HTTP\/1\.1\" (\d{3}) (\d+)$"
-    return re.match(pattern, line)
 
 
 total_file_size = 0
@@ -33,26 +17,29 @@ status_codes = {
     "500": 0
 }
 
-try:
-    for line in sys.stdin:
-        if line_count == 10:
-            print(f"File size: {total_file_size}")
-            for code, count in status_codes.items():
-                if count > 0:
-                    print(f"{code}: {count}")
-            line_count = 0
-
-        if validate(line):
-            line_count += 1
-            total_file_size += int(line.split()[-1])
-            code = line.split()[-2]
-            status_codes[code] = status_codes[code] + 1
-        else:
-            line_count += 1
-            pass
-
-except KeyboardInterrupt:
+def print_stat():
+    """
+    prints the metrics
+    """
     print(f"File size: {total_file_size}")
     for code, count in status_codes.items():
         if count > 0:
             print(f"{code}: {count}")
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        for line in sys.stdin:
+            if line_count == 10:
+                print_stat()
+                line_count = 0
+
+            line_count += 1
+            total_file_size += int(line.split()[-1])
+            code = line.split()[-2]
+            status_codes[code] = status_codes[code] + 1
+
+    except KeyboardInterrupt:
+        print_stat()
