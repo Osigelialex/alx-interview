f0c0f84a6e7eb28513fb5abdb35306e76387df4c
Osigelialex <osigelialex@gmail.com>
2023-07-20 19:43:15 +0100

    Task fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
 """
-A module that solves the log parsing problem
+A module for solvin the log_parsing problem
 """
 import sys
 
 
-def parse_line(line):
-    """extracts data from line"""
-    line = line.split()
-    if len(line) < 7:
-        return None, None
-    file_size = line[-1]
-    status_code = line[-2]
-    return int(file_size), status_code
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+total_file_size = 0
 
 
-def print_stats(codes, file_size):
-    """prints the metrics"""
-    print("File size: {}".format(file_size))
-    for status_code in sorted(codes.keys()):
-        code = codes[status_code]
-        if isinstance(code, int) and code > 0:
-            print("{}: {}".format(status_code, code))
+def print_stats():
+    """displays the metrics"""
+    print("File size: {}".format(total_file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code]:
+            print("{}: {}".format(status, status_codes[code]))
 
 
-def main():
-    """main function"""
-    total_file_size = 0
+if __name__ == "__main__":
     line_count = 0
-    status_codes = {}
-
     try:
         for line in sys.stdin:
-            file_size, code = parse_line(line)
-            line_count += 1
-
-            if code and file_size:
-                total_file_size += file_size
-                status_codes[code] = status_codes.get(code, 0) + 1
-
-            if line_count % 10 == 0 and line_count > 0:
-                print_stats(status_codes, total_file_size)
+            try:
+                comp = line.split()
+                total_file_size += int(comp[-1])
+                if comp[-2] in status_codes:
+                    status_codes[comp[-2]] += 1
+            except Exception:
+                pass
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
     except KeyboardInterrupt:
-        print_stats(status_codes, total_file_size)
-
-
-if __name__ == '__main__':
-    main()
+        print_stats()
+        raise
+    print_stats()
