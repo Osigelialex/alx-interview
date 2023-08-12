79afcec6f5e1a07af17a04a7b6ec38e1481329d1
Osigelialex <osigelialex@gmail.com>
2023-08-12 09:57:04 +0100

    added solution

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const process = require('process');
+
+const movieID = process.argv[2];
+const api = 'https://swapi-api.alx-tools.com/api/';
+const filmsEndPoint = `${api}films/${movieID}`;
+
+request(filmsEndPoint, (error, response, data) => {
+  if (!error && response.statusCode === 200) {
+    const json = JSON.parse(data);
+    const characterEndPoints = json.characters;
+
+    function printCharacterNames (index) {
+      if (index >= characterEndPoints.length) return;
+
+      request(characterEndPoints[index], (charError, charResponse, charData) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const json = JSON.parse(charData);
+          const characterName = json.name;
+          console.log(characterName);
+        }
+      });
+
+      printCharacterNames(++index);
+    }
+
+    printCharacterNames(0);
+  }
+});
