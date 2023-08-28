7b0c409ae2edbad7f59df8fb2c42c91603987292
Osigelialex <osigelialex@gmail.com>
2023-08-28 19:01:33 +0100

    add solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module for solving island perimeter problem
+"""
+from typing import List
+
+
+def island_perimeter(grid: List[List]):
+    visited = set()
+
+    def dfs(i, j):
+        if grid[i][j] == 0:
+            return 1
+
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        perim = dfs(i - 1, j)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i, j + 1)
+        return perim
+
+    for i in range(1, len(grid[0]) - 1):
+        for j in range(1, len(grid) - 1):
+            if grid[i][j] == 1:
+                return dfs(i, j)
